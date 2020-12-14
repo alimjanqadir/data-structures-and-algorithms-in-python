ab656460c3344ca902ed3665db1b30c7ff25b6d3
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2020-12-14 15:07:16 +0800

    LinkedList insert and print_list methods implemented.

--- data_structures/linked_list.py
+++ data_structures/linked_list.py
@@ -10,14 +10,28 @@ class LinkedList:
         self.head = 0
         self.tail = 0
 
-    def insert(self):
-        pass
+    def insert(self, node: Node, position=-1):
+        if len(self.list) == 0:
+            self.list.append(node)
+        elif position == -1:
+            previous_node = self.list[len(self.list) - 1]
+            previous_node.next = node
+            self.list.append(node)
+        else:
+            temp = self.list[position]
+            previous_node = self.list[position - 1]
+            previous_node.next = node
+            node.next = temp
+        self.tail += 1
 
     def remove(self):
         pass
 
     def print_list(self):
-        pass
+        while self.head < self.tail:
+            node = self.list[self.head]
+            print(node.value)
+            self.head += 1
 
 
 a = Node('a')
@@ -28,3 +42,10 @@ a.next = b
 b.next = c
 a.next = b
 a.next = b
+
+linked_list = LinkedList()
+linked_list.insert(a)
+linked_list.insert(b)
+linked_list.insert(c)
+linked_list.insert(d)
+linked_list.print_list()
