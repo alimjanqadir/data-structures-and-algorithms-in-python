87678bacf5346f3baf644e7e54748b84b855a9a2
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2021-01-20 09:40:03 +0800

    Binary search finished.

--- algorithms/binary_search.py
+++ algorithms/binary_search.py
@@ -1,29 +1,25 @@
 def binary_search(input_array, value):
-    """Your code goes here."""
     input_length = len(input_array)
     if input_length == 0:
         return -1
 
-    left = 0
-    right = input_length - 1
-    while left <= right:
-        mid_point = (left + right) / 2
-        value_in_middle = input_array[mid_point]
+    sorted_array = sorted(input_array)
+    start = 0
+    end = input_length - 1
+    print(sorted_array)
+    while start <= end:
+        mid_point = (start + end) // 2
+        value_in_middle = sorted_array[mid_point]
 
-        if value_in_middle == value:
-            return mid_point
-
-        if value_in_middle < value:
-            left = mid_point + 1
-        elif value_in_middle > value:
-            right = mid_point - 1
+        if value < value_in_middle:
+            end = mid_point - 1
+        elif value > value_in_middle:
+            start = mid_point + 1
         else:
             return mid_point
     return -1
 
 
-test_list = [-4, 1, 3, 9, 11, 15, 19, 29, 43, 55, 234, 34234, 144444]
-test_val1 = -4
-test_val2 = 15
-print(binary_search(test_list, test_val1))
+test_list = [2, 5, 7, 9, 12]
+test_val2 = 12
 print(binary_search(test_list, test_val2))
