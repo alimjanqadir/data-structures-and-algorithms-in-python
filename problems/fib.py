bf268090720448efe14a92070363c50b6300d42a
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2021-01-31 13:19:40 +0800

    Fibonacci generator refactored.

--- problems/fib.py
+++ problems/fib.py
@@ -2,7 +2,7 @@ def fib(n):
     counter = 0
     a, b = 0, 1
     print(a, b, end=' ')
-    while counter < n:
+    while counter < n - 1:
         print(a + b, end=' ')
         t = a + b
         a = b
@@ -10,13 +10,22 @@ def fib(n):
         counter += 1
 
 
-def fib_recursive(n):
+def fib_recursive_helper(n):
     if n == 0:
         return 0
     elif n == 1:
         return 1
     else:
-        result = fib_recursive(n - 2) + fib_recursive(n - 1)
+        result = fib_recursive_helper(n - 2) + fib_recursive_helper(n - 1)
         return result
 
 
+def fib_recursive(n):
+    if n > 0:
+        fib_recursive(n - 1)
+    print(fib_recursive_helper(n), end=' ')
+
+
+fib_recursive(10)
+print()
+fib(10)
