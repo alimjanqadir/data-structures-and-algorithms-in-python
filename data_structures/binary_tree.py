793d8a96bea4c3a584c6da24fb3c9f72fc8ad2c3
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2021-03-28 13:16:15 +0800

    Binary tree implementing begin.

---
+++ data_structures/binary_tree.py
@@ -0,0 +1,19 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+
+    def put_value(self, value):
+        pass
+
+    def print_tree(self):
+        pass
+
+    def search(self, value):
+        pass
