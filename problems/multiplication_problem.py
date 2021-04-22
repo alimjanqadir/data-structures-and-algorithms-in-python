02e0d6b20359328243938429d77dc3311f6e7bc1
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2021-04-22 09:02:32 +0800

    PRACTICE: multiplication problem.

--- problems/multiplication_problem.py
+++ problems/multiplication_problem.py
@@ -8,4 +8,17 @@ def naive(a, b):
     return z
 
 
-print(naive(10, 2))
+def russian(a, b):
+    x = a
+    y = b
+    z = 0
+    while x > 0:
+        if x % 2 == 1: z +=y
+        x = x >> 1
+        y = y << 1
+    return z
+
+def russian_rec(a, b):
+    if a == 0: return 0
+    if a % 2 == 0: return 2 * russian_rec(a / 2, b)
+    return b + 2 * russian_rec((a - 1) / 2, b)
